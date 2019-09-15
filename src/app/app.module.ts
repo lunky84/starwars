84d08b41f8d076d5efcafc3e531f29ae4d7a4faf
John Downie <john@bluestone360.co.uk>
2019-09-15 22:43:07 +0100

    components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TabsComponent } from './tabs/tabs.component';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TabsComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
