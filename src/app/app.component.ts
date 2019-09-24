c4130143955e091994cc2735f6a452a27c91a40a
John Downie <john@bluestone360.co.uk>
2019-09-24 21:45:30 +0100

    http get request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { StarWarsService } from './star-wars.service';
 
 @Component({
@@ -7,6 +7,16 @@ import { StarWarsService } from './star-wars.service';
   styleUrls: ['./app.component.scss'],
   providers: [StarWarsService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'project';
+
+  swService: StarWarsService;
+
+  constructor (swService:StarWarsService) {
+    this.swService = swService;
+  }
+
+  ngOnInit() {
+    this.swService.fetchCharacters();
+  }
 }
