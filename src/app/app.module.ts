9fd6a73af25ecba0fe1e9b2f0a804562e4fdeb1e
John Downie <john@bluestone360.co.uk>
2019-09-25 22:29:50 +0100

    splitting code into modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { TabsComponent } from './tabs/tabs.component';
@@ -10,18 +9,8 @@ import { StarWarsService } from './star-wars.service';
 import { LogService } from './log.service';
 import { CreateCharacterComponent } from './create-character/create-character.component';
 import { HeaderComponent } from './header/header.component';
-import { RouterModule } from '@angular/router';
 import { HttpModule } from '@angular/http';
-
-const routes = [
-  {path: 'characters', component: TabsComponent, children: [
-    {path: '', redirectTo: 'all', pathMatch: 'full'},
-    {path: ':side', component: ListComponent}
-  ]},
-  {path: 'new-character', component: CreateCharacterComponent},
-  {path: '**', redirectTo: '/'}
-];
-
+import { AppRoutingModule } from './app.routing.module';
 
 @NgModule({
   declarations: [
@@ -29,14 +18,12 @@ const routes = [
     TabsComponent,
     ListComponent,
     ItemComponent,
-    CreateCharacterComponent,
     HeaderComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    RouterModule.forRoot(routes),
-    HttpModule
+    HttpModule,
+    AppRoutingModule
   ],
   providers: [StarWarsService, LogService],
   bootstrap: [AppComponent]
